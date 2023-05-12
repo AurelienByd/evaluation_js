97764fc2d32a90bf875a7b59a71e8dc2360f886c
aurelien <rubis91999@hotmail.com>
2023-05-12 13:10:59 +0200

    evaluation

--- Exercice_1/javascript.js
+++ Exercice_1/javascript.js
@@ -1,1 +1,10 @@
-console.log("Connecté !");
+var table = ["-20 ans", "Entre 20 et 40 ans", "+40 ans"];
+var age = parseInt(window.prompt("Saisir votre âge :"));
+while (age<=100)
+{
+    if (age===100)
+    {
+        break;
+    }
+}
+console.table(table);
