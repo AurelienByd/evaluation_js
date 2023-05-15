35ad86f6f0322d73a8ee15bba4c3b029a7c90470
aurelien <rubis91999@hotmail.com>
2023-05-15 10:56:18 +0200

    javascript evaluation exercice 1 end

--- Exercice_1/javascript.js
+++ Exercice_1/javascript.js
@@ -1,10 +1,38 @@
-var table = ["-20 ans", "Entre 20 et 40 ans", "+40 ans"];
+var o = 1;
+var q = 1;
+var p = 1;
+var age1;
+var age2;
+var age3;
+var tab1 = [];
+tab1[0] = ["Moins de 20 ans"];
+tab1[o] = [age];
+var tab2 = [];
+tab2[0] = ["Entre 20 et 40 ans"];
+tab2[q] = [age];
+var tab3 = [];
+tab3[0] = ["Plus de 40 ans"];
+tab3[p] = [age];
 var age = parseInt(window.prompt("Saisir votre âge :"));
-while (age<=100)
+while (age<100)
 {
-    if (age===100)
+    age = parseInt(window.prompt("Saisir votre âge :"));
+    if (age<20)
     {
-        break;
+    tab1[o] = [age + " an(s)"];
+    o++;
+    }
+    if (age>=20 && age<=40)
+    {
+    tab2[q] = [age + " an(s)"];
+    q++;
+    }
+    if (age>40)
+    {
+    tab3[p] = [age + " an(s)"];
+    p++;
     }
 }
-console.table(table);
+console.table(tab1);
+console.table(tab2);
+console.table(tab3);
