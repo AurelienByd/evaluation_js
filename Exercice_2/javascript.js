5f4c855b07130a96704da4939c81b5044ee77713
aurelien <rubis91999@hotmail.com>
2023-05-15 11:40:54 +0200

    javascript evaluation exercice 2 end

---
+++ Exercice_2/javascript.js
@@ -0,0 +1,7 @@
+let m = 0;
+var TableMultiplication = parseInt(window.prompt("Entrer un nombre :"));
+while (m<=20)
+{
+    console.log(m + " x " + TableMultiplication + " = " +(TableMultiplication*m));
+    m++;
+}
