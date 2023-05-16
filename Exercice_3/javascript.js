cfe77aaad30a4ed707ffd29f0a16177bba56ce35
aurelien <rubis91999@hotmail.com>
2023-05-16 12:33:12 +0200

    javascript evaluation

--- Exercice_3/javascript.js
+++ Exercice_3/javascript.js
@@ -1,3 +1,11 @@
 var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
-
+var a = tab.indexOf((window.prompt("Ecrire un prénom")));
+if (a!=-1)
+{
+    delete tab[a];
+}
+else if (a==-1)
+{
+    window.alert("ERREUR!");
+}
 console.table(tab);
